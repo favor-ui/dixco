7eab7d364c4711767cf6c7dbdaee6a28f0afbe89
favor-ui <fervplore@gmail.com>
2020-04-28 09:26:49 +0100

    intial commit

--- app/views.py
+++ app/views.py
@@ -40,35 +40,35 @@ def get_all_transactions():
 
 
 
-        
+
 @app.route('/disco_transaction', methods = ['POST'])
 # @jwt_required
 def register():
     """To add an item to the database. its a post request and we use databasename.insert()"""
     try:
         transactions = mongo.db.transactions_
-        
-        account_number1 = request.json['account_number']
+        request_data = request.get_json()
+        account_number1 = request_data['account_number']
         account_number= int(account_number1)
         if not account_number:
             return jsonify({"Error":"Field can not be blank", "status":0})
         
-        account_type1 = request.json['account_type']
+        account_type1 = request_data['account_type']
         account_type = str(account_type1)
         if not account_type:
             return jsonify({"Error":"Field can not be blank", "status":0})
-       
-        transaction_val1 = request.json['transaction_value']
+        
+        transaction_val1 = request_data['transaction_value']
         transaction_val = int(transaction_val1)
         if not transaction_val:
             return jsonify({"Error":"Field can not be blank", "status":0})
-       
-        transaction_ref1 = request.json['transaction_ref']
-        transaction_ref = str(transaction_ref)
+        
+        transaction_ref1 = request_data['transaction_ref']
+        transaction_ref = str(transaction_ref1)
         if not transaction_ref:
             return jsonify({"Error":"Field can not be blank", "status":0})
         
-        token1 = request.json['token']
+        token1 = request_data['token']
         token = str(token1)
         if not token:
             return jsonify({"Error":"Field can not be blank", "status":0})
